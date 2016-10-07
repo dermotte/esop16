028644700d87cc1465914fb0b140184fcc1b8a8c
dermotte <mlux@itec.aau.at>
2016-10-07 14:04:44 +0200

    variables

---
+++ src/at/aau/itec/esop16/lesson02/Variables.java
@@ -0,0 +1,12 @@
+package at.aau.itec.esop16.lesson02;
+
+/**
+ * @author Dr. Mathias Lux, mlux@itec.aau.at, 07.10.2016 13:59.
+ */
+public class Variables {
+    int numberOfPeople;
+    int x,y,z;
+    byte temp;
+    int anInt;
+
+}
